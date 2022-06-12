e2439c48b5e5b26f1ba4b003ba376d2b1d778199
Anubhav Sahu <anubhavsahu71@gmail.com>
2022-06-12 21:35:52 +0530

    added few lines to show output

--- timeconversion.py
+++ timeconversion.py
@@ -20,18 +20,24 @@ import sys
 
 def timeConversion(s):
     # Write your code here
-    if s[-2:] == "AM" and s[:2] == "12": 
+    if s[-2:]=="AM" and s[:2]=="12": 
         return "00" + s[2:-2] 
-    elif s[-2:] == "AM": 
+    elif s[-2:]=="AM": 
         return s[:-2]
-    elif s[-2:] == "PM" and s[:2] == "12": 
+    elif s[-2:]=="PM" and s[:2]=="12": 
         return s[:-2] 
     else:
-       ans = int(s[:2]) + 12
-       return str(str(ans) + s[2:8]) 
+       ans = int(s[:2])+12
+       return str(str(ans)+s[2:8]) 
         
 
 if __name__ == '__main__':
+    f = open(os.environ['OUTPUT_PATH'], 'w')
+
     s = input()
 
     result = timeConversion(s)
+
+    f.write(str(result) + '\n')
+
+    f.close()
